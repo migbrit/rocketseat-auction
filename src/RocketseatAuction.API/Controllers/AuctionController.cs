4fb865233122694ae08a0b0abfae4df835fa5245
Miguel Brito <miguel.brito@levva.io>
2024-02-06 01:04:15 -0300

    feat: adding swagger documentation and if no content

--- src/RocketseatAuction.API/Controllers/AuctionController.cs
+++ src/RocketseatAuction.API/Controllers/AuctionController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using RocketseatAuction.API.Entities;
 using RocketseatAuction.API.UseCases.Auctions.GetCurrent;
 
 namespace RocketseatAuction.API.Controllers;
@@ -13,9 +14,14 @@ public class AuctionController : ControllerBase
     }
 
     [HttpGet]
+    [ProducesResponseType(typeof(Auction), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
     public IActionResult GetCurrentAction()
     {
         var result = _getCurrentAuctionUseCase.Execute();
+
+        if(result is null)
+            return NoContent();
 
         return Ok(result);
     }
