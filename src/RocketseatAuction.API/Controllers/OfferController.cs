29e2b62750d2fbc8ccfc81f262f0d0a376752520
Miguel Brito <miguel.brito@levva.io>
2024-02-06 20:16:25 -0300

    feat: creating offer controller

---
+++ src/RocketseatAuction.API/Controllers/OfferController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RocketseatAuction.API.Http.Requests;
+
+namespace RocketseatAuction.API.Controllers;
+public class OfferController : RocketseatAuctionBaseController
+{
+    [HttpPost]
+    [Route("{itemId}")]
+    public IActionResult CreateOffer([FromRoute] int itemId, [FromBody] RequestCreateOffer request)
+    {
+        return Created();
+    }
+}
